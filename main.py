5718b579e7e7876b509226fdef4395c202047cb5
Aleksandra Jonas <jonas.aleksandra@gmail.com>
2022-11-09 00:27:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 def main():
     today = datetime.now()
-     iso = today.isoformat()
-      dt = iso[:-7] + '+00:00'
-      print(dt)
+    iso = today.isoformat()
+    dt = iso[:-7] + '+00:00'
+    print(dt)
 
 if __name__ == "__main__":
     main()
