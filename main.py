b62c2b7388f8ed0f14b644cf4122f4f4be199816
Aleksandra Jonas <jonas.aleksandra@gmail.com>
2022-11-13 15:59:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from flask import Flask
-from datetime import datetime
+import datetime
+from time import localtime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    today = datetime.now()
-    iso = today.isoformat()
-    date = iso[:-7] + '+00:00'
+    today = datetime.datetime.now()
+    lt = localtime()
+    tz = int(lt.tm_gmtoff/(60*60))
 
-    return date
+    iso = today.isoformat()
+    dat = iso[:-7]
+
+    if tz < 0:
+        dat += '-'
+    else: 
+        dat += '+'
+    
+    if tz < 10: 
+        dat += '0'
+
+    dat += str(tz) + ':00'
+
+    return dat
 
 if __name__ == "__main__":
     app.run()
